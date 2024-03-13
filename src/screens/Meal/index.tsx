c5bebb69c6d96438ac77d49dd78b0ddaecc916fe
Demis Russo <demisjunior@hotmail.com>
2024-03-13 16:42:25 -0300

    add meal form

--- src/screens/Meal/index.tsx
+++ src/screens/Meal/index.tsx
@@ -1,12 +1,40 @@
+import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Statistics } from '@/components/Statistics'
-import { Content } from './styles'
+import { Content, Form, FormColumn, FormRow, Input, Label } from './styles'
+import { Button } from '@/components/Button'
+import { ConfirmationButton } from '@/components/ConfirmationButton'
 
 export function Meal() {
   const { navigate } = useNavigation()
 
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('')
+
+  const [focusedButton, setFocusedButton] = useState<null | string>(null)
+
   function handleNavigateToHome() {
     navigate('home')
+  }
+
+  function handleClickSelectedButton(button: string) {
+    setFocusedButton(button)
+  }
+
+  function handleSubmit() {
+    try {
+      const meal = {
+        name,
+        description,
+        date,
+        time,
+        diet: focusedButton,
+      }
+    } catch {
+      
+    }
   }
 
   return (
@@ -18,7 +46,74 @@ export function Meal() {
         variant='default'
       />
 
-      <Content></Content>
+      <Content>
+        <Form>
+          <FormColumn>
+            <Label>Nome</Label>
+            <Input
+              keyboardAppearance='dark'
+              enterKeyHint='done'
+              value={name}
+              onChangeText={setName}
+            />
+          </FormColumn>
+
+          <FormColumn>
+            <Label>Descrição</Label>
+            <Input
+              keyboardAppearance='dark'
+              enterKeyHint='done'
+              expandedView
+              value={description}
+              onChangeText={setDescription}
+            />
+          </FormColumn>
+
+          <FormRow>
+            <FormColumn style={{ flex: 1 }}>
+              <Label>Data</Label>
+              <Input
+                keyboardAppearance='dark'
+                enterKeyHint='done'
+                keyboardType='decimal-pad'
+                value={date}
+                onChangeText={setDate}
+              />
+            </FormColumn>
+
+            <FormColumn style={{ flex: 1 }}>
+              <Label>Hora</Label>
+              <Input
+                keyboardAppearance='dark'
+                enterKeyHint='done'
+                keyboardType='decimal-pad'
+                value={time}
+                onChangeText={setTime}
+              />
+            </FormColumn>
+          </FormRow>
+
+          <FormColumn>
+            <Label>Está dentro da dieta?</Label>
+
+            <FormRow>
+              <ConfirmationButton
+                text='Sim'
+                variant={focusedButton === 'sim' ? 'primary' : 'default'}
+                onPress={() => handleClickSelectedButton('sim')}
+              />
+
+              <ConfirmationButton
+                text='Não'
+                variant={focusedButton === 'não' ? 'secondary' : 'default'}
+                onPress={() => handleClickSelectedButton('não')}
+              />
+            </FormRow>
+          </FormColumn>
+        </Form>
+
+        <Button text='Cadastrar Refeição' onPress={handleSubmit} />
+      </Content>
     </>
   )
 }
--- src/screens/Meal/styles.ts
+++ src/screens/Meal/styles.ts
@@ -1,12 +1,42 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
 import styled from 'styled-components/native'
+
+interface InputProps {
+  expandedView?: boolean;
+  isFocused?: boolean;
+}
 
 export const Content = styled.View`
   padding: 24px;
   flex: 1;  
   background-color: ${({ theme }) => theme.colors.gray7};
   border-radius: 20px;
-  margin-top: -10px;
-  align-items: center;
+  margin-top: -20px;
   gap: 16px;
+`
+
+export const Form = styled.View`
+  flex: 1;
+`
+
+export const FormColumn = styled.View`
+  margin-bottom: 24px;
+`
+
+export const FormRow = styled.View`
+  flex-direction: row;
+  gap: 16px;
+`
+
+export const Label = styled.Text`
+  color: ${({ theme }) => theme.colors.gray2};
+  font-size: ${({ theme }) => theme.fontSize.sm}px;
+  font-family: ${({ theme }) => theme.fontFamily.bold};
+  margin-bottom: 6px;
+`
+
+export const Input = styled.TextInput<InputProps>`
+  height: ${({ expandedView }) => expandedView ? 120 : 48}px;
+  border: 1px solid ${({ theme, isFocused }) => isFocused ? theme.colors.gray1 : theme.colors.gray5};
+  border-radius: 6px;
+  padding: 0 24px;
 `
